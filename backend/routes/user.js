91faabadf829cc1105828eee7af6e5bb607c41b0
Pankaj Singh <pankaj5202x@gmail.com>
2024-09-07 11:06:44 +0530

    routes updated

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -83,3 +83,30 @@ router.put("/", authMiddlware, async (req, res) => {
     msg: "Updated successfully",
   });
 });
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
